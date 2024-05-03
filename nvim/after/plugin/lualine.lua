9b84ce33dcea8321cc806ebd0e5f8a08f4da4b36
Goncalo Antunes <>
2024-05-03 02:48:41 +0100

    Updated LuaLine Config

--- nvim/after/plugin/lualine.lua
+++ nvim/after/plugin/lualine.lua
@@ -1,5 +1,19 @@
 require('lualine').setup({
+    options = {
+        section_separators = { left = '', right = '' },
+        component_separators = { left = '', right = '' },
+        theme = 'catppuccin-mocha',
+    },
+    sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch" },
+        lualine_c = { "diff" },
+        lualine_d = { "filename" },
 
-    options = { theme = 'catppuccin-mocha' }
-}
-)
+        lualine_w = { "progress" },
+        lualine_x = { "filetype" },
+        lualine_y = { "progress" },
+        lualine_z = { "location" },
+    }
+
+})
